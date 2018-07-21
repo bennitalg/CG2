f26cece300a92c816feaf3efce55127d0419e765
bennitalg <bennitalg@outlook.com>
2018-07-20 23:13:58 -0500

    Please review

--- RectangleArea/Program.cs
+++ RectangleArea/Program.cs
@@ -10,6 +10,19 @@ namespace RectangleArea
     {
         static void Main(string[] args)
         {
+            int area, length, width;
+            Console.Write("Please write the length of your rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please write the width of your rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
+            Console.WriteLine("The area of rectangle : {0}", area);
+            Console.ReadKey();
+
+
+            
+
+            
         }
     }
 }
